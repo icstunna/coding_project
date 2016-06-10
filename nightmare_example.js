9004afbabb0d6f1399c767cd770e8ebee9a11f62
icstunna <mdelostr@gmail.com>
2016-06-09 17:19:53 -0700

    collected list of search results

--- nightmare_example.js
+++ nightmare_example.js
@@ -10,19 +10,16 @@ nightmare
   .evaluate(function () {
     var returnList = []
     returnList.push(document.querySelector('#resultStats').innerHTML);
-    returnList.push(document.querySelector('h3 > a').innerHTML);
+    returnList.push(document.querySelector('.srg').children);
     return returnList
   })
   .end()
   .then(function (result) {
-    var regex = /\d\d\d,\d\d\d/;
-    var intString = result[0].match(regex)[0]
-    output["number_of_results"] = intString
-    output["title_of_search_results"] = result
-    // console.log(output)
-    for(i = 0; i < result.length; i++) {
-      console.log(result[i])
-    }
+    // var regex = /\d\d\d,\d\d\d/;
+    // var intString = result[0].match(regex)[0]
+    // output["number_of_results"] = intString
+    // output["the_rest"] = result
+    console.log(result)
   })
   .catch(function (error) {
     console.error('Search failed:', error);
