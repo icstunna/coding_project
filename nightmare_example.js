16c2917164ac20e3c03acd83fb4cf9767e1b76ed
icstunna <mdelostr@gmail.com>
2016-06-10 23:45:41 -0700

    actual json is now written on the data file, still am not able to retrieve data for page 2 nor consolidate it

--- nightmare_example.js
+++ nightmare_example.js
@@ -3,11 +3,10 @@ var nightmare = Nightmare({ show: true });
 var jsonfile = require('jsonfile');
 var file = './data.json'
 var output = new Object();
-// var returnList = new Array();
 
 
 
-function page1() {
+function run() {
   nightmare
   .goto('http://google.com')
   .type('form[action="/search"] [name=q]', 'captain 401k')
@@ -17,8 +16,8 @@ function page1() {
     var returnList = [];
     var titles = [];
     returnList.push(document.querySelector('#resultStats').innerHTML);
-    titles.push(document.querySelector('.r > a').innerHTML); //the first title is unaccessible by conventional means
-    var titlesPartTwo = document.querySelector('.srg').children; //this grabs the container for remaining search results
+    titles.push(document.querySelector('.r > a').innerHTML); //the first title is unaccessible otherwise
+    var titlesPartTwo = document.querySelector('.srg').children; //this grabs the container of search results
     for (i = 0; i < titlesPartTwo.length; i++) {
       var html = titlesPartTwo[i].children[0].children[0].children[0].innerHTML //this accesses the titles
       titles.push(html)
@@ -27,34 +26,20 @@ function page1() {
     returnList.push(titles)
     return returnList
   })
-  .click('.pn')
-  .wait('#resultStats')
-  .evaluate(function() {
-    var titles = []
-    var titlesPartTwo = document.querySelector('.srg').children; //this grabs the container for remaining search results
-    for (i = 0; i < titlesPartTwo.length; i++) {
-      var html = titlesPartTwo[i].children[0].children[0].children[0].innerHTML //this accesses the titles
-      titles.push(html)
-    }
-
-    return titles
-  })
   .end()
   .then(function (result) {
-    // var regex = /\d\d\d,\d\d\d/;
-    // var intString = result[0].match(regex)[0]
-    // output['number_of_results'] = intString
-    // output['title_of_search_results'] = result[1]
-    // // console.log(output)
-    // // var x = JSON.stringify(output)
-    // jsonfile.writeFile(file, output, function(err) {
-    //   console.log(err)
-    // })
-    console.log(result)
+    var regex = /\d\d\d,\d\d\d/;
+    var intString = result[0].match(regex)[0]
+    output['number_of_results'] = intString
+    output['title_of_search_results'] = result[1]
+    var json_output = JSON.stringify(output)
+    jsonfile.writeFile(file, json_output, function(err) {
+      console.log(err)
+    })
   })
   .catch(function (error) {
     console.error('Search failed:', error);
   });
 }
 
-page1();
+run();
