93257ec330f7f7afe93c572f67701953039f3c33
icstunna <mdelostr@gmail.com>
2016-06-11 19:24:11 -0700

    created commentary for why I am not able to scrape page two of the Google search results. also mildly refactored the code in order to make clear the purpose of specific variables

--- nightmare_example.js
+++ nightmare_example.js
@@ -3,18 +3,18 @@ var nightmare = Nightmare({ show: true });
 var jsonfile = require('jsonfile');
 var file = './data.json'
 var output = new Object();
+var scrapeResults = new Array();
 
 
 function run() {
-  var list = []
   nightmare
   .goto('http://google.com')
   .type('form[action="/search"] [name=q]', 'captain 401k')
   .click('form[action="/search"] [type=submit]')
   .wait('#resultStats')
-  .evaluate(function (list) {
-    var titles = [];
-    list.push(document.querySelector('#resultStats').innerHTML);
+  .evaluate(function (scrapeResults) {
+    var titles = new Array;
+    scrapeResults.push(document.querySelector('#resultStats').innerHTML); //the number of search results
     titles.push(document.querySelector('.r > a').innerHTML); //the first title is unaccessible otherwise
     var titlesPartTwo = document.querySelector('.srg').children; //this grabs the container of search results
     for (i = 0; i < titlesPartTwo.length; i++) {
@@ -22,24 +22,32 @@ function run() {
       titles.push(html)
     }
 
-    list.push(titles)
-    return list
-  }, list)
+    scrapeResults.push(titles)
+    return scrapeResults
+  }, scrapeResults)
+  /*********************************************
+  This was my attempt to click to page two
+  and scrape the second page. While the scrape
+  was successful, I cannot determine how to store
+  the scraped results unto the 'scrapeResults'
+  variable in order to have access to both the
+  page one and page two search result titles
+  *********************************************/
   // .click('.pn')
   // .wait(1000)
-  // .evaluate(function(list) {
+  // .evaluate(function(scrapeResults) {
   //   var titlesPartTwo = document.querySelector('.srg').children; //this grabs the container of search results
   //   for (i = 0; i < titlesPartTwo.length; i++) {
   //     var html = titlesPartTwo[i].children[0].children[0].children[0].innerHTML //this accesses the titles
-  //     list.push(html)
+  //     scrapeResults.push(html)
   //   }
-  //   return list
-  // }, list)
+  //   return scrapeResults
+  // }, scrapeResults)
   .end()
   .then(function (result) {
     var regex = /\d\d\d,\d\d\d/;
-    var intString = result[0].match(regex)[0]
-    output['number_of_results'] = intString
+    var resultStats = result[0].match(regex)[0]
+    output['number_of_results'] = resultStats
     output['title_of_search_results'] = result[1]
     jsonfile.writeFile(file, output)
     // console.log(result)
